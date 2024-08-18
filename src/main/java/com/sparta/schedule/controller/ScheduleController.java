169bb197c3a0bca187d81f66a7844e087da4ed1b
Kang IWon <esfne4727@naver.com>
2024-08-18 22:12:35 +0900

    3단계 일정 목록 조회

--- src/main/java/com/sparta/schedule/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedule/controller/ScheduleController.java
@@ -9,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/schedule")
@@ -29,5 +32,15 @@ public class ScheduleController {
         }
         log.info("Get by Id: {}", id);
         return ResponseEntity.ok(scheduleServiceImpl.getSchedule(id));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Schedule>> getByNameAndModDate(@RequestParam(required = false) String name, @RequestParam(required = false)LocalDate modDate) {
+        List<Schedule> schedules = scheduleServiceImpl.getScheduleByNameAndModDate(name, modDate);
+        log.info("Get list by name: {}, modDate: {}",name,modDate);
+        if (schedules.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(schedules);
     }
 }
--- src/main/java/com/sparta/schedule/service/ScheduleService.java
+++ src/main/java/com/sparta/schedule/service/ScheduleService.java
@@ -4,9 +4,14 @@ import com.sparta.schedule.Dto.ScheduleRequestDto;
 import com.sparta.schedule.Dto.ScheduleResponseDto;
 import com.sparta.schedule.entity.Schedule;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ScheduleService {
     Schedule createSchedule(ScheduleRequestDto scheduleRequestDto);
 
     ScheduleResponseDto getSchedule(Long id);
+
+    List<Schedule> getScheduleByNameAndModDate(String name, LocalDate modDate);
 
 }
